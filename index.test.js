34cb7267521940328c68b102da39c4a09bae8501
‘AMAYA <aamaya547@gmail.com>
2022-08-09 10:58:22 -0700

    tried our best

--- index.test.js
+++ index.test.js
@@ -3,7 +3,9 @@ describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
     const stack = new Stack();
     stack.push(1);
+    //peek returns the first element on that list
     expect(stack.peek()).toEqual(1);
   });
+
   // add more tests here...
 });
